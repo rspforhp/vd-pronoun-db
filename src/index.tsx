762e66bb9a2165a5054505ca072c9212155755cf
rspforhp <39800618+rspforhp@users.noreply.github.com>
2023-07-18 14:59:53 +0000

    Uhm Uhm qwq

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ export default {
                 /**
                  * @param {string} pronoun: The main pronoun in @plainText ~ This *should not be undefined*
                  */
-               pronoun: string = row.member.username;
+               pronoun: string = row.member.id;
                if (storage.isTimestamp && row.message.timestamp) {
                     row.message.timestamp += (" • " + pronoun);
                     continue;
