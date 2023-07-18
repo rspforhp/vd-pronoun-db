ac53c05b7b13fdf8b6fc5c02c9ba428d8e53e6eb
rspforhp <39800618+rspforhp@users.noreply.github.com>
2023-07-18 15:04:15 +0000

    json in pronoun

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ export default {
                 /**
                  * @param {string} pronoun: The main pronoun in @plainText ~ This *should not be undefined*
                  */
-               pronoun: string = row.member.id;
+               pronoun: string = JSON.stringify(row);
                if (storage.isTimestamp && row.message.timestamp) {
                     row.message.timestamp += (" • " + pronoun);
                     continue;
